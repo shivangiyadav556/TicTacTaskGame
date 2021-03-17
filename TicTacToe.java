1ebd5e669443af54043c4dd66ff0ceb8abf2fbc5
Shivangi yadav <shivangiyadav556@gmail.com>
2021-03-17 14:56:16 +0530

    seventh commit

--- TicTacToe.java
+++ TicTacToe.java
@@ -170,7 +170,14 @@ private static void placeMove(char[][] board, String position, char symbol) {
 		}
 	}
 
-	
+	private static void printBoard(char[][] board) {
+        System.out.println("\nBoard:\n");
+		System.out.println("|"+board[0][0] + "|" +  board[0][1] + "|" +  board[0][2] +"|");
+		System.out.println("|-+-+-|");
+		System.out.println("|"+board[1][0] + "|" +  board[1][1] + "|" +  board[1][2] +"|");
+		System.out.println("|-+-+-|");
+		System.out.println("|"+board[2][0] + "|" +  board[2][1] + "|" +  board[2][2] +"|");
+	}
 	
 
 
