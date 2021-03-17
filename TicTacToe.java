499daf2c77f8394883a5e1c4973341e07abaaaba
Shivangi yadav <shivangiyadav556@gmail.com>
2021-03-17 14:49:29 +0530

    second commit

--- TicTacToe.java
+++ TicTacToe.java
@@ -54,6 +54,21 @@ private static boolean isGameFinished(char[][] board) {
 		System.out.println("The game ended in a tie!");
 		return true;
 	}
+private static boolean hasContestantWon(char[][] board, char symbol) {
+		if ((board[0][0] == symbol && board [0][1] == symbol && board [0][2] == symbol) ||
+			(board[1][0] == symbol && board [1][1] == symbol && board [1][2] == symbol) ||
+			(board[2][0] == symbol && board [2][1] == symbol && board [2][2] == symbol) ||
+			
+			(board[0][0] == symbol && board [1][0] == symbol && board [2][0] == symbol) ||
+			(board[0][1] == symbol && board [1][1] == symbol && board [2][1] == symbol) ||
+			(board[0][2] == symbol && board [1][2] == symbol && board [2][2] == symbol) ||
+			
+			(board[0][0] == symbol && board [1][1] == symbol && board [2][2] == symbol) ||
+			(board[0][2] == symbol && board [1][1] == symbol && board [2][0] == symbol) ) {
+			return true;
+		}
+		return false;
+	}
 
 
 
